28479f5772e8b7eadc8ce6cc17b9ac740d604634
Milica Bulajic <milica.bulajic@gmail.com>
2022-01-12 14:00:41 +0100

    sequelize configuration

---
+++ server/config/config.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
+
+module.exports = {
+  "development": {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: "classify_dev",
+    host: DB_HOST,
+    dialect: "postgres",
+    port: "5433",
+  },
+  "test": {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: "classify_test",
+    host: DB_HOST,
+    dialect: "postgres",
+    port: "5433",
+  },
+  "production": {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: "classify_prod",
+    host: DB_HOST,
+    dialect: "postgres",
+    port: "5433",
+  }
+};
+
